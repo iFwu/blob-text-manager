34579ac922ba35c6113a59583b67046996f08521
iFwu <blanu@live.com>
2024-11-23 17:39:40 +0800

    fix: reduce the complexity of the code

--- app/actions.ts
+++ app/actions.ts
@@ -53,19 +53,12 @@ export async function getBlob(url: string): Promise<string> {
 
 export async function putBlob(name: string, content: string | File | null) {
   const { blobs } = await list();
-  const existingFile = blobs.find(
+  const existingFiles = blobs.filter(
     (blob) => blob.pathname.replace(/-[a-zA-Z0-9]{21}(\.[^.]+)?$/, '$1') === name
   );
 
-  if (!existingFile) {
-    return createNewFile(name, content);
-  }
-
-  const oldVersions = blobs.filter(
-    (blob) => blob.pathname.replace(/-[a-zA-Z0-9]{21}(\.[^.]+)?$/, '$1') === name
-  );
-
-  for (const oldVersion of oldVersions) {
+  // 如果文件已存在，删除所有旧版本
+  for (const oldVersion of existingFiles) {
     await del(oldVersion.url);
   }
 
@@ -75,23 +68,10 @@ export async function putBlob(name: string, content: string | File | null) {
   } else {
     content = handleEmptyContent(content, name);
   }
+  
   const result = await put(name, content, {
     access: 'public',
-    addRandomSuffix: true,
-  });
-  return result;
-}
-
-async function createNewFile(name: string, content: string | File | null) {
-  const isFolder = name.endsWith('/');
-  if (isFolder) {
-    content = new File([], name, { type: 'application/x-empty' });
-  } else {
-    content = handleEmptyContent(content, name);
-  }
-  const result = await put(name, content, {
-    access: 'public',
-    addRandomSuffix: true,
+    addRandomSuffix: !isFolder,
   });
   return result;
 }
